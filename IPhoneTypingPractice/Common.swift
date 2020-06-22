92d43ce49b46f38b11435258628f46b5a18dc849
devyhan93 <devyhan93@gmail.com>
2020-06-22 16:59:27 +0900

    git init

---
+++ IPhoneTypingPractice/Common.swift
@@ -0,0 +1,13 @@
+//
+//  Common.swift
+//  IPhoneTypingPractice
+//
+//  Created by 요한 on 2020/06/22.
+//  Copyright © 2020 요한. All rights reserved.
+//
+
+import UIKit
+
+struct Common {
+  // Make common Attribute
+}
